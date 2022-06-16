e1f1ff58bd3ed0d8046b6a7f817134b9338f05a2
manu-2509 <maneshs111@gmail.com>
2022-06-16 19:52:42 +0530

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./index')
 
 const connect = require('./configs/db')
 
-app.listen(2000,async()=>{
+app.listen(process.env.PORT||2000,async()=>{
     try {
        await connect();
         
