7cd508baa27faecc4e40f5a82c7dec236f96004c
manu-2509 <maneshs111@gmail.com>
2022-06-16 19:06:54 +0530

    test

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connect=()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/library")
+    return mongoose.connect("mongodb+srv://9061202920:9061202920@cluster0.4vy0z.mongodb.net/?retryWrites=true&w=majority")
 }
 
 module.exports = connect;
